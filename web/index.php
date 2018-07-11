36ebcfea91821c699f8d8bff429029ba05e026f8
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-11 16:15:19 +0300

    logging

--- web/index.php
+++ web/index.php
@@ -1,25 +1,18 @@
 <?php
 
-require('../vendor/autoload.php');
+error_log("hello, this is a test!");
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+//define("PASSWORD","speaknfun");
+//
+///*
+//PAYMENT_ID:PAYEE_ACCOUNT:PAYMENT_AMOUNT:PAYMENT_UNITS:PAYMENT_BATCH_NUM:PAYER_ACCOUNT:PasswordMD5:TIMESTAMPGMT
+//*/
+//
+//$string=
+//    $_POST['PAYMENT_ID'].':'.$_POST['PAYEE_ACCOUNT'].':'.$_POST['PAYMENT_AMOUNT'].':'.$_POST['PAYMENT_UNITS'].':'.
+//    $_POST['PAYMENT_BATCH_NUM'].':'.$_POST['PAYER_ACCOUNT'].':'.strtoupper(md5(PASSWORD)).':'.$_POST['TIMESTAMPGMT'];
+//
+//$v2key=$_POST['V2_HASH'];
+//$hash=strtoupper(md5($string));
+//if($hash==$v2key) echo ("ok. we got payment");
+//if($hash==$v2key) mail("speaknfun@yopmail.com", "ok. we got payment", "Line 1\nLine 2\nLine 3");
