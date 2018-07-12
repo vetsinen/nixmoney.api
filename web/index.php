e694680d9f956e1ddcea22d3dd3392088f9db61c
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-12 14:10:11 +0300

    iinterkassa header

--- web/index.php
+++ web/index.php
@@ -1,4 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="interkassa-verification" content="e60b9d2ba474deddebd30869305e5442" />
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 echo 'index.php';
 error_log("index.php launched!");
+?>
+</body>
+</html>
