865015045a56f5ee0c4ad21e7986864baa73bf3c
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-12 14:12:31 +0300

    shelter in title

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="interkassa-verification" content="e60b9d2ba474deddebd30869305e5442" />
-    <title>Document</title>
+    <title>shelter index.php</title>
 </head>
 <body>
 <?php
