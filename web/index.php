fd8e080dd162af033cfec32f679d4be659151241
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-13 10:29:59 +0300

    form added

--- web/index.php
+++ web/index.php
@@ -14,5 +14,13 @@
 echo 'this is shelter';
 error_log("index.php launched!");
 ?>
+<form id="payment" name="payment" method="post" action="https://sci.interkassa.com/" enctype="utf-8">
+    <input type="hidden" name="ik_co_id" value="5b4736333d1eaf7c298b456b" />
+    <input type="hidden" name="ik_pm_no" value="ID_4233" />
+    <input type="hidden" name="ik_am" value="100.00" />
+    <input type="hidden" name="ik_cur" value="UAH" />
+    <input type="hidden" name="ik_desc" value="ticket to club" />
+    <input type="submit" value="Pay">
+</form>
 </body>
 </html>
